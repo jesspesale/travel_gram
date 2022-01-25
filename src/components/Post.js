e2f60799341e07633921d50e7b03144f9d444dab
jesspesale <jess_pesale@aol.com>
2022-01-24 23:24:47 -0500

    added basic react image to work on post styling

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,9 +4,8 @@ function Post() {
     return (
         <div>
             <h3>Username</h3>
-            {/* Header => Avatar => username */}
-            {/* Image */}
-            {/* username / caption */}
+            <img src="https://www.freecodecamp.org/news/content/images/2021/06/Ekran-Resmi-2019-11-18-18.08.13.png" />
+            <h4>Username:  caption</h4>
         </div>
     );
 }
