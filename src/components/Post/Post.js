38dbf16035dd05a9b3377e4a9d2841d9540a7e92
jesspesale <jess_pesale@aol.com>
2022-01-24 23:42:14 -0500

    installed material UI and added a avatar with letter of username to start

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import "./Post.css"
+import { Avatar } from '@mui/material';
 
 function Post() {
     return (
         <div className='post'>
+            <div className='post_header'>
             <h3>Username: Jess_Pesale</h3>
+                <Avatar
+                    className="post_avatar"
+                    alt="JessPesale"
+                    src="/static/images/avatar/1.jpg"
+                />
+            </div>
         <img className='post_image' src="https://www.freecodecamp.org/news/content/images/2021/06/Ekran-Resmi-2019-11-18-18.08.13.png" />
           <h4 className='post_text'><strong>Jess_Pesale</strong> caption game strong </h4> 
         </div>
