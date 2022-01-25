947471774163f18ddd2033ab158f9d8152255205
jesspesale <jess_pesale@aol.com>
2022-01-24 23:09:21 -0500

    added navbar and background styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className="app_navbar">
+        <p>TravelGram</p>
+
+      </div>
+
+      {/* NavBar/ Header */}
+      {/* Posts */}
       <h2>Hi there</h2>
     </div>
-  );
+  )
 }
 
 export default App;
