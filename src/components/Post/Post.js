bdbdfc5802210d4600a21b6167e8cea3467cbfc7
jesspesale <jess_pesale@aol.com>
2022-01-25 23:48:19 -0500

    rendered 3 different posts through props

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./Post.css"
 import { Avatar } from '@mui/material';
 
-function Post() {
+function Post({username, caption, imgUrl}) {
     return (
         <div className='post'>
             <div className='post_header'>
@@ -11,10 +11,10 @@ function Post() {
                     alt="JessPesale"
                     src="/static/images/avatar/1.jpg"
                     />
-                <h3>Username: Jess_Pesale</h3>
+                <h3>Username: {username}</h3>
             </div>
-        <img className='post_image' src="https://www.freecodecamp.org/news/content/images/2021/06/Ekran-Resmi-2019-11-18-18.08.13.png" />
-        <h4 className='post_text'><strong>Jess_Pesale</strong> caption game strong </h4> 
+        <img className='post_image' src={imgUrl} />
+        <h4 className='post_text'><strong>{username}</strong> {caption} </h4> 
         </div>
     );
 }
