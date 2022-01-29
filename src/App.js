9b1c4953bccd90f4e8e9adae0e885c9d43bff7f7
jesspesale <jess_pesale@aol.com>
2022-01-28 19:36:24 -0500

    added new material ui version, added modal and styles from website with variable and useState

--- src/App.js
+++ src/App.js
@@ -5,10 +5,35 @@ import { db } from './firebase';
 import {collection, onSnapshot } from "firebase/firestore";
 //  getDocs,  ^^
 import Modal from '@mui/material/Modal';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+// import Button from '@mui/material/Button';
+import { makeStyles } from '@material-ui/core/styles';
+// import Typography from '@mui/material/Typography';
+
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        position: 'absolute',
+        width: 450,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
 
 function App() {
+  const classes = useStyles()
+  // turns the useStyle function above into a constant
+  const [modalStyle] = useState(getModalStyle)
+
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false)
 
@@ -24,18 +49,16 @@ function App() {
 
   return (
     <div className="app">
-      <Modal
-        open={open}
-        onClose={() => setOpen(false)}
-  // on close listens for any clicks outside of the Modal
-      >
-      <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-      </Typography>   
-      </Modal>
-
-
-      <div className="app_navbar">
+            <Modal
+                open={open}
+                onClose={() => setOpen(false)}
+            >
+          <div style={modalStyle} className={classes.paper}>
+            <h2>I am a Modal </h2>
+          </div>
+          </Modal>
+          
+      <div className="app_navbar" >
         <p>TravelGram</p>
       </div>
         {
