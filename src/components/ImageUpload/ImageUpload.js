b32a96d237f687825e0fb316d64fd2f10393a1ca
jesspesale <jess_pesale@aol.com>
2022-02-01 22:20:27 -0500

    trying to add the new image to firestore - need new syntax on firebase.firestore

--- src/components/ImageUpload/ImageUpload.js
+++ src/components/ImageUpload/ImageUpload.js
@@ -2,19 +2,20 @@ import { Button } from '@mui/material';
 import React, {useState} from "react"
 import {storage, db} from "../../firebase.js"
 import {ref} from "firebase/storage"
+import {username} from "../Modal/ModalElement"
 
-function ImageUpload() {
+function ImageUpload({username, setUsername}) {
 
-    const [image, setImage] = useState(null)
-    const [progress, setProgress] = useState(0)
+    const [image, setImage] = useState(null);
+    const [progress, setProgress] = useState(0);
 //progress bar to show how much of the image has been uploaded
-    const [caption, setCaption] = useState("")
+    const [caption, setCaption] = useState("");
 
 
     const handleUpload = () => {
 //access storage in fb and get a reference to the photo you selected
-        const uploadTask = ref(storage, `images/${image.name}`).put(image)
-                            //putting the image you ^ grabbed into that point
+        const uploadTask = ref(storage, `images/${image.name}`).put(image);
+                            //uploadiing the image
 
         uploadTask.on(
 // on state change give me a snapshot
@@ -25,10 +26,30 @@ function ImageUpload() {
     // works out a number 1-100 & set progess level to that number
                     (snapshot.bytesTransferred/snapshot.totalBytes) * 100
                 );
-                setProgress(progress)
+                setProgress(progress);
+            },
+            (error) => {
+            // tells you if there was any problems/errors w/ the upload
+                console.log(error);
+                alert(error.message);
+            },
+            () => {
+                storage
+                .ref("images")      // go to the ref image
+                .child(image.name)  // go to the img names child
+                .getDownloadURL()   // DL URL is needed to for post submission
+                .then(url => {
+                // post img in database
+                    db.collection("posts").add({
+                        // timestamp:  firebase.firestore.FieldValue.serverTimestamp(),
+                        caption:    caption,
+                        imgUrl:     url,
+                        username:   username   
+                    });
+                })
             }
         )
-    }
+    };
 
     const handleChange = (e) => {
         if (e.target.files[0]) {
